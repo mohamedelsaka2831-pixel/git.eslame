89e47f3718a6bab396fd272595f5e352f0a1e8b6
elsaka <mohamedelsaka2831@gmail.com>
2026-07-28 05:54:21 +0300

    Add

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:eslame_assignment/ui/home/home_screen.dart';
+import 'package:eslame_assignment/utils/app_routes.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute:AppRoutes.homeRouteName ,
+      routes:{
+        AppRoutes.homeRouteName : (context)=>HomeScreen(),
+      }
+    );
+  }
+}
